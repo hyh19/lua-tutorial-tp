7830c0005f43e60141031843a7d2e780557bc4c5
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-09 08:11:44 +0000

    上传代码

---
+++ lua_tables/sample1.lua
@@ -0,0 +1,10 @@
+-- sample table initialization
+mytable = {}
+
+-- simple table value assignment
+mytable[1] = "Lua"
+
+-- removing reference
+mytable = nil
+
+-- lua garbage collection will take care of releasing memory
