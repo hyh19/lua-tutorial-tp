89535e6e3e49ce7edeeceb0dd24c8f1a18830858
Yuhui Huang <mrhuangyuhui@gmail.com>
2022-01-10 08:18:12 +0000

    更新代码

--- lua_coroutines/sample2.lua
+++ lua_coroutines/sample2.lua
@@ -32,3 +32,17 @@ end
 for index = 1, 10 do
     print(index, getNumber())
 end
+
+--[[
+$ lua sample2.lua
+1       1
+2       2
+3       3
+4       4
+5       5
+6       1
+7       2
+8       3
+9       4
+10      5
+]]
